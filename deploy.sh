669d011d00b64d703b33a748a8d0c70760553ce8
Christian Nilsson <christian.nilsson.4@wirelesscar.com>
2019-08-19 20:36:50 +0200

    Settings

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ echo "# Kill"
 kill $(cat app.pid)
 
 echo "# Pull"
-cd repo/tattoo
+cd tattoo
 git pull
 
 echo "# Build"
 mvn clean install -DskipTests
 
 echo "# Start"
-cd ../..
-java -jar repo/tattoo/target/tattoo-1.0-SNAPSHOT.jar > /dev/null &
+cd ..
+java -jar tattoo/target/tattoo-1.0-SNAPSHOT.jar > /dev/null &
